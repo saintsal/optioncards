d3bf42bb1a29783966a7c00583c83315d1e777a2
Salim Virani <smile@saintsal.com>
2018-06-09 13:01:50 +0300

    reload from backup

---
+++ assets/js/theme.js
@@ -0,0 +1,28 @@
+
+    // carousel demo
+
+!function ($) {
+
+    $('#myCarousel').carousel()
+    
+}(window.jQuery)
+
+!function ($) {
+
+    $('#myCarousel2').carousel({
+    	interval: false
+    })
+
+    
+}(window.jQuery)
+
+
+!function ($) {
+
+    $('#myCarousel3').carousel({
+    	interval: false
+    })
+
+
+    
+}(window.jQuery)
